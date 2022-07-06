2f803e3d46a023d0a050fcabff42b69d44f19747
Jacks Win10 <jackson.calixto@hotmail.com>
2022-07-06 07:33:06 -0300

    Starting The project

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Exemplo de Get";
+        }
+
+        [HttpPost]
+        public string PodeSerPostOuOutroNomeDeFuncao()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string PutOuOutroNomeDeFuncao(int id)
+        {
+            return "Exemplo de Put";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteOuOutroNomeDeFuncao(int id)
+        {
+            return "Exemplo de Delete";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,7 +8,27 @@ C# XML Documentation Comments - Keisuke Kato
 NuGet Gallery - pcislo
 .NET Core Tools - Jun Han
 .NET Core Test Explorer - Jun Han
+GitLens — Git supercharged - GitKraken
 Ctrl+P
 >OmniSharp: Select Project
 If not working, is being recommended to install another version
 C# v1.23.17
+
+In the Front App, run...
+NPM INSTALL
+After that run...
+ng serve -o
+
+In the Back, run...
+dotnet tool uninstall --global dotnet-ef
+dotnet tool install --global dotnet-ef --version 5.0.2
+dotnet tool list -g
+D:\Code\ProEventos\Back\src> dotnet ef database update -s .\ProEventos.API\
+dotnet watch run -p .\ProEventos.API\
+
+Starting from Zero
+dotnet new globaljson --sdk-version 5.0.408 --force
+
+dotnet new webapi -n 
+
+Postman review
