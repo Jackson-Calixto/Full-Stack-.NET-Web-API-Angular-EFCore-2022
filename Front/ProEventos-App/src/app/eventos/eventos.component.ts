effbc4d5fa5d4284b3470811f6c17679692b7387
Jacks Win10 <jackson.calixto@hotmail.com>
2022-07-25 05:55:42 -0300

    Applying Filters

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { response } from 'express';
 
@@ -9,15 +10,36 @@ import { response } from 'express';
 })
 export class EventosComponent implements OnInit {
 
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor =  filtrarPor.toLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+                       evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
+
   public eventos: any;
+  public eventosFiltrados: any;
   widthImg = 150;
   marginImg = 2;
   exibirImagem = true;
-  filtroBusca = '';
+  private _filtroBusca = '';
+
+  public get filtroBusca():string {
+    return this._filtroBusca;
+  }
+
+  public set filtroBusca(value: string) {
+    this._filtroBusca = value;
+    this.eventosFiltrados = this.filtroBusca ? this.filtrarEventos(this.filtroBusca) : this.eventos;
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
