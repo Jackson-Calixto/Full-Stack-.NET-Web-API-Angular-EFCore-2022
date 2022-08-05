4ada860a23a0c31e16b14ba9b8a9d9f9ba21c274
Jacks Win10 <jackson.calixto@hotmail.com>
2022-08-05 07:46:08 -0300

    Strict Template & TSConfig

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,10 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { ToastrService } from 'ngx-toastr';
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
-import { TituloComponent } from '../../shared/titulo/titulo.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
