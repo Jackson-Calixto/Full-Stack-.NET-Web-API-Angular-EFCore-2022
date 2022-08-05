c83a72cad51b26f96c77cc87bcbcf635a22c4fb0
Jacks Win10 <jackson.calixto@hotmail.com>
2022-08-05 07:15:54 -0300

    Show Menu & Mini Logo

--- Front/ProEventos-App/src/app/shared/nav/nav.component.ts
+++ Front/ProEventos-App/src/app/shared/nav/nav.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
   isCollapsed = true;
   display = 'block';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showMenu() {
+    return this.router.url !== '/user/login';
   }
-
 }
