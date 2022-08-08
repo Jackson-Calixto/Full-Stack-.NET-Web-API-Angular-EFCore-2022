2de361f085fef2af909c7d275e8fd20568873b0d
Jacks Win10 <jackson.calixto@hotmail.com>
2022-08-08 20:00:22 -0300

    DTOs conclusion

--- Back/src/ProEventos.Application/helpers/ProEventosProfile.cs
+++ Back/src/ProEventos.Application/helpers/ProEventosProfile.cs
@@ -8,6 +8,9 @@ namespace ProEventos.Application.helpers
     {
         public ProEventosProfile(){
             CreateMap<Evento, EventoDto>().ReverseMap();
+            CreateMap<Lote, LoteDto>().ReverseMap();
+            CreateMap<Palestrante, PalestranteDto>().ReverseMap();
+            CreateMap<RedeSocial, RedeSocial>().ReverseMap();
         }        
     }
 }
