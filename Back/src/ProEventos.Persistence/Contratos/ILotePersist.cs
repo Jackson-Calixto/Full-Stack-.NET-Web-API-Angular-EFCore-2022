bd3a83d4a45daae54fed2fcbbc1080264180cd22
Jacks Win10 <jackson.calixto@hotmail.com>
2022-08-10 08:26:37 -0300

    Persistence Lote

--- Back/src/ProEventos.Persistence/Contratos/ILotePersist.cs
+++ Back/src/ProEventos.Persistence/Contratos/ILotePersist.cs
@@ -13,13 +13,13 @@ namespace ProEventos.Persistence.Contratos
         /// </summary>
         /// <param name="eventoId">Código chave da tabela Evento</param>
         /// <returns>Array de Lotes</returns>
-        Task<Lote> GetLotesByEventoIdAsync(int eventoId);
+        Task<Lote[]> GetLotesByEventoIdAsync(int eventoId);
         /// <summary>
         /// Método get que retornará apenas 1 Lote
         /// </summary>
         /// <param name="eventoId">Código chave da tabela Evento</param>
         /// <param name="loteId">Código chave da tabela Lote</param>
         /// <returns>Apenas 1 Lote</returns>
-        Task<Lote[]> GetLoteByIdAsync(int eventoId, int loteId);
+        Task<Lote> GetLoteByIdAsync(int eventoId, int loteId);
     }
 }
