19b6d1d3da031f1ab107c41e972f24899edfa846
Jacks Win10 <jackson.calixto@hotmail.com>
2022-08-10 08:11:05 -0300

    Interface Lote

---
+++ Back/src/ProEventos.Persistence/Contratos/ILotePersist.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contratos
+{
+    public interface ILotePersist
+    {
+        /// <summary>
+        /// Método get que retornará 1 lista de Lotes por Evento
+        /// </summary>
+        /// <param name="eventoId">Código chave da tabela Evento</param>
+        /// <returns>Array de Lotes</returns>
+        Task<Lote> GetLotesByEventoIdAsync(int eventoId);
+        /// <summary>
+        /// Método get que retornará apenas 1 Lote
+        /// </summary>
+        /// <param name="eventoId">Código chave da tabela Evento</param>
+        /// <param name="loteId">Código chave da tabela Lote</param>
+        /// <returns>Apenas 1 Lote</returns>
+        Task<Lote[]> GetLoteByIdAsync(int eventoId, int loteId);
+    }
+}
