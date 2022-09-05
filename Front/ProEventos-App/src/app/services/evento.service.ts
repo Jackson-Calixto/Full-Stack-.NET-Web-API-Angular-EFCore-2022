6fad97551118b25f71e33045b7353612f4c3514f
Jacks Win10 <jackson.calixto@hotmail.com>
2022-09-05 07:46:56 -0300

    Using Hard coded Token on http request

--- Front/ProEventos-App/src/app/services/evento.service.ts
+++ Front/ProEventos-App/src/app/services/evento.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable, take } from 'rxjs';
@@ -11,11 +11,14 @@ import { Evento } from '../models/Evento';
 export class EventoService {
 
   baseURL = environment.apiURL + 'api/eventos';
+  tokenHeader = new HttpHeaders({
+    'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJqYWNrc29uIiwibmJmIjoxNjYyMzY0MzI5LCJleHAiOjE2NjI0NTA3MjksImlhdCI6MTY2MjM2NDMyOX0.3iYTGFIOidX3eLf7jLDMLVwD5PTXIYA-RKMvTyWMKeL5Q-DiN8QAqRr6gEf0VYmJw2yjnsoywd-01trXG7J4YA'
+  });
 
   constructor(private http: HttpClient) {}
 
   public GetEventos(): Observable<Evento[]> {
-    return this.http.get<Evento[]>(this.baseURL).pipe(take(1));
+    return this.http.get<Evento[]>(this.baseURL, {headers: this.tokenHeader}).pipe(take(1));
   }
 
   public GetEventosByTema(tema: string): Observable<Evento[]> {
