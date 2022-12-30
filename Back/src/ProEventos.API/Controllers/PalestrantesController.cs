378af2110510aff44e2bc96e211ffa44252c700b
Jacks Win10 <jackson.calixto@hotmail.com>
2022-12-30 14:15:33 -0300

    Palestrante - Controller

---
+++ Back/src/ProEventos.API/Controllers/PalestrantesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Application.Contratos;
+using Microsoft.AspNetCore.Http;
+using ProEventos.Application.Dtos;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Linq;
+using ProEventos.API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using ProEventos.Persistence.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PalestrantesController : ControllerBase
+    {
+        private readonly IPalestranteService _palestranteService;
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IAccountService _accountService;
+
+        public PalestrantesController(IPalestranteService palestranteService, 
+                                 IWebHostEnvironment hostEnvironment,
+                                 IAccountService accountService)
+        {
+            _palestranteService = palestranteService;
+            _hostEnvironment = hostEnvironment;
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]PageParams pageParams)
+        {
+            try
+            {
+                var palestrantes = await _palestranteService.GetAllPalestrantesAsync(User.GetUserId(), pageParams, true);
+                if (palestrantes == null) return NoContent();
+
+                Response.AddPagination(palestrantes.CurrentPage, palestrantes.PageSize, palestrantes.TotalCount, palestrantes.TotalPages);
+
+                return Ok(palestrantes);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar palestrantes. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var palestrante = await _palestranteService.GetPalestranteByIdAsync(User.GetUserId(), id, true);
+                if (palestrante == null) return NoContent();
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar palestrante por id. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("tema/{tema}")]
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PalestranteAddDto model)
+        {
+            try
+            {
+                var palestrante = await _palestranteService.GetPalestranteByIdAsync(User.GetUserId(), model.Id, false);
+                
+                if (palestrante == null) 
+                    palestrante = await _palestranteService.AddPalestrante(User.GetUserId(), model);
+                
+                if (palestrante == null) return BadRequest("Erro ao tentar adicionar palestrante.");
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar adicionar palestrante. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, PalestranteUpdateDto model)
+        {
+            try
+            {
+                var palestrante = await _palestranteService.UpdatePalestrante(User.GetUserId(), model);
+                if (palestrante == null) return BadRequest("Erro ao tentar atualizar palestrante.");
+
+                return Ok(palestrante);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar atualizar palestrante. Erro: {ex.Message}");
+            }
+        }
+    }
+}
