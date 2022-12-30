5a6f2e039e1b7367558d968967efd05ce9b01186
Jacks Win10 <jackson.calixto@hotmail.com>
2022-12-30 08:06:20 -0300

    Redes Sociais - Classe Persistencia

--- Back/src/ProEventos.Persistence/Contratos/IRedeSocialPersist.cs
+++ Back/src/ProEventos.Persistence/Contratos/IRedeSocialPersist.cs
@@ -9,7 +9,7 @@ namespace ProEventos.Persistence.Contratos
     public interface IRedeSocialPersist: IGeralPersist
     {
         Task<RedeSocial> GetRedeSocialEventoByIdsAsync(int eventoId, int id);
-        Task<RedeSocial> GetRedeSocialPalestranteByIdsAsync(int eventoId, int id);
+        Task<RedeSocial> GetRedeSocialPalestranteByIdsAsync(int palestranteId, int id);
         Task<RedeSocial[]> GetAllByEventoIdAsync(int eventoId);
         Task<RedeSocial[]> GetAllByPalestranteIdAsync(int palestranteId);
     }
